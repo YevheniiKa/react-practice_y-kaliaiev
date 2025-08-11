50a1afd1f131d79b5753a95cc7ec98b5fe3bef56
Yevhenii Kaliaiev <eugenyr34@gmail.com>
2025-08-11 11:52:54 +0300

    fifth commit

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,9 @@ export const App = () => {
   const [selectUser, setUser] = useState('all');
   const [query, setQuery] = useState('');
   const lowerQuery = query.toLowerCase().trim();
-  const [isReverse, setIsReverse] = useState(false);
+  const [sortState, setSortState] = useState('none');
+  const [selectedCategories, setSelectedCategories] = useState(['all']);
+
   const products = productsFromServer.map(product => {
     const category = categoriesFromServer.find(
       c => c.id === product.categoryId,
@@ -32,8 +34,29 @@ export const App = () => {
       selectUser === 'all' ||
       (product.user && String(product.user.id) === selectUser);
 
-    return matchesQuery && matchesUser;
+    const matchesCategory =
+      selectedCategories.includes('all') ||
+      (product.category && selectedCategories.includes(product.category.title));
+
+    return matchesQuery && matchesUser && matchesCategory;
   });
+
+  function toggleCategory(categoryTitle) {
+    if (categoryTitle === 'all') {
+      setSelectedCategories(['all']);
+    } else {
+      setSelectedCategories(prev => {
+        if (prev.includes('all')) return [categoryTitle];
+        if (prev.includes(categoryTitle)) {
+          const filtered = prev.filter(c => c !== categoryTitle);
+
+          return filtered.length === 0 ? ['all'] : filtered;
+        }
+
+        return [...prev, categoryTitle];
+      });
+    }
+  }
 
   return (
     <div className="section">
@@ -56,7 +79,8 @@ export const App = () => {
               {usersFromServer.map(user => {
                 return (
                   <a
-                    data-cy="FilterAllUsers"
+                    key={user.id}
+                    data-cy="FilterUser"
                     href="#/"
                     className={cn({
                       'is-active': selectUser === `${user.id}`,
@@ -76,6 +100,7 @@ export const App = () => {
                   type="text"
                   className="input"
                   placeholder="Search"
+                  value={query}
                   onChange={event => setQuery(event.currentTarget.value)}
                 />
 
@@ -85,11 +110,14 @@ export const App = () => {
 
                 <span className="icon is-right">
                   {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-                  <button
-                    data-cy="ClearButton"
-                    type="button"
-                    className="delete"
-                  />
+                  {query && (
+                    <button
+                      data-cy="ClearButton"
+                      type="button"
+                      className="delete"
+                      onClick={() => setQuery('')}
+                    />
+                  )}
                 </span>
               </p>
             </div>
@@ -98,16 +126,23 @@ export const App = () => {
               <a
                 href="#/"
                 data-cy="AllCategories"
-                className="button is-success mr-6 is-outlined"
+                className={cn('button mr-6 ', {
+                  'is-success': selectedCategories.includes('all'),
+                })}
+                onClick={() => toggleCategory('all')}
               >
                 All
               </a>
               {categoriesFromServer.map(category => {
                 return (
                   <a
+                    key={category.id}
                     href="#/"
                     data-cy="AllCategories"
-                    className="button is-success mr-2 is-outlined"
+                    className={cn('button mr-2 ', {
+                      'is-info': selectedCategories.includes(category.title),
+                    })}
+                    onClick={() => toggleCategory(category.title)}
                   >
                     {category.title}
                   </a>
@@ -120,6 +155,12 @@ export const App = () => {
                 data-cy="ResetAllButton"
                 href="#/"
                 className="button is-link is-outlined is-fullwidth"
+                onClick={event => {
+                  event.preventDefault();
+                  setUser('all');
+                  setSelectedCategories(['all']);
+                  setQuery('');
+                }}
               >
                 Reset all filters
               </a>
@@ -141,9 +182,7 @@ export const App = () => {
                       <span className="icon">
                         <i
                           data-cy="SortIcon"
-                          className={cn('fas', 'fa-sort', {
-                            'fa-sort-down': isReverse,
-                          })}
+                          className="fas fa-sort fa-sort-down"
                         />
                       </span>
                     </a>
@@ -192,23 +231,30 @@ export const App = () => {
             </thead>
 
             <tbody>
-              {visibleProducts.map(product => (
-                <tr key={product.id} data-cy="Product">
-                  <td className="has-text-weight-bold" data-cy="ProductId">
-                    {product.id}
-                  </td>
-                  <td data-cy="ProductName">{product.name}</td>
-                  {product.category && (
-                    <td data-cy="ProductCategory">{`${product.category.icon} - ${product.category.title}`}</td>
-                  )}
-                  {product.user && (
-                    <td data-cy="ProductUser" className="has-text-danger">
-                      {product.user.name}
+              {visibleProducts.length > 0 ? (
+                visibleProducts.map(product => (
+                  <tr key={product.id} data-cy="Product">
+                    <td className="has-text-weight-bold" data-cy="ProductId">
+                      {product.id}
                     </td>
-                  )}
-                </tr>
-              ))}
-              {visibleProducts.length === 0 && (
+                    <td data-cy="ProductName">{product.name}</td>
+                    {product.category && (
+                      <td data-cy="ProductCategory">{`${product.category.icon} - ${product.category.title}`}</td>
+                    )}
+                    {product.user && (
+                      <td
+                        data-cy="ProductUser"
+                        className={cn({
+                          'has-text-link': product.user.sex === 'm',
+                          'has-text-danger': product.user.sex === 'f',
+                        })}
+                      >
+                        {product.user.name}
+                      </td>
+                    )}
+                  </tr>
+                ))
+              ) : (
                 <p data-cy="NoMatchingMessage">
                   <br />
                   No products matching selected criteria
